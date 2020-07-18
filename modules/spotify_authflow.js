d79c427339bfb9a0ab9b0e39b3189c061ee3b551
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-07-18 09:52:44 +0200

    Init

---
+++ modules/spotify_authflow.js
@@ -0,0 +1,32 @@
+let a
+
+let generateRandomString = function(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+
+let login_route = function(req, res) {
+    let stateKey = 'auth_state';
+    let state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    let scope = 'streaming user-read-private user-read-email user-read-playback-state user-modify-playback-state';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        })
+    );
+};
+
+
