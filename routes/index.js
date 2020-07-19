3ea7990ead1b8836418d8b5e8c1f29c741dcee08
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-07-19 17:27:51 +0200

    Add routes for login logic, callback logic and accessToken retrieval

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+let spotify_authflow = require('../modules/spotify_authflow');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//
+// });
+
+router.get('/login', function(req, res, next) {
+  spotify_authflow.login(req, res, next);
+});
+
+router.get('/callback', function(req, res, next) {
+  spotify_authflow.callback(req, res, next);
+});
+
+router.post('/accessToken', function (req, res, next) {
+  let data = {
+    'access_token': req.session.access_token
+  };
+  res.type('application/json');
+  res.status(200);
+  res.send(data)
 });
 
 module.exports = router;
