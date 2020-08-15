1c1e88a1f3780d6d93341acda7ab84c21d0f2071
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-08-15 11:22:51 +0200

    Remove debug remainders, create song get route and prepare post route

--- routes/player.js
+++ routes/player.js
@@ -3,11 +3,6 @@ let db = require('../modules/mongodb_handler');
 var router = express.Router();
 
 router.get('/playstate', async (req, res, next) => {
-    let db_token = await db.getToken('access_token');
-    let token = {
-        name: db_token.name,
-        value: db_token.value
-    };
     let paused_event = req.query.paused;
     let paused_state = req.session.paused;
     if(paused_state === undefined)
@@ -20,25 +15,28 @@ router.get('/playstate', async (req, res, next) => {
 
 
     }
-
-    console.log(req.query);
     res.status(200);
     res.send('ok')
 });
 
 
-router.get('/songs/:id', (req, res, next) => {
-    let b = req.params.id;
-    res.status(200);
-    res.send('ok')
+router.get('/songs/:id', async (req, res, next) => {
+    let song_id = req.params.id;
+    let has_song = await db.hasSong(song_id);
+    if(has_song){
+        res.status(200);
+    } else {
+        res.status(404);
+    }
+    res.send();
 });
 
 
 router.post('/songs/:id', (req, res, next) => {
-    let b = req.body;
-    console.log(b);
+    let song_id = req.params.id;
+    console.log('Add song: ', song_id);
     res.status(200);
-    res.send('ok')
+    res.send();
 });
 
 module.exports = router;
