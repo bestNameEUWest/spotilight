70d0f1fe538fc5a1feec8ba698791f8e161f8dad
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-08-15 11:17:27 +0200

    Create db song model

---
+++ models/db_Song.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const SongSchema = mongoose.Schema({
+    version: {
+        type: Number,
+        required: true
+    },
+
+    id: {
+        type: String,
+        required: true
+    },
+
+    bars: [{
+        start: { type: Number },
+        duration: { type: Number }
+    }],
+
+    beats: [{
+        start: { type: Number },
+        duration: { type: Number }
+    }],
+
+    sections: [{
+        start: { type: Number },
+        duration: { type: Number },
+        loudness: { type: Number },
+        tempo: { type: Number },
+        key: { type: Number },
+
+    }],
+
+    segments: [{
+        start: { type: Number },
+        duration: { type: Number },
+        loudness_start: { type: Number },
+        pitches: [Number],
+        timbre: [Number]
+    }],
+
+});
+
+module.exports = mongoose.model('Songs', SongSchema);
