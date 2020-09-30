678c6ef28ce2a20bfcf14a83a26b4cfc0457e344
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-09-30 14:28:05 +0200

    Abstract handler to enable multiple song analysis later

--- modules/audio_analysis_handler.js
+++ modules/audio_analysis_handler.js
@@ -1,18 +1,16 @@
-const Song = require('../models/db_Song');
 const request = require('request');
 let db = require('../modules/mongodb_handler');
 let _ = require('lodash');
 
 const version = '1';
 
-async function getSelectionFor(json, selection) {
-    return await _.map(json, function(entry) {
+function getSelectionFor(json, selection) {
+    return _.map(json, function (entry) {
         return _.pick(entry, selection)
     });
 }
 
-
-async function getAudioAnalysisForSongID(id) {
+async function getAudioAnalysisForATrack(id) {
     let url = 'https://api.spotify.com/v1/audio-analysis/';
     let access_token = (await db.getAccessToken()).value;
     let options = {
@@ -21,24 +19,39 @@ async function getAudioAnalysisForSongID(id) {
         json: true
     };
 
-    await request.get(options, async function (error, res, body) {
-        let song = new Song();
-        song.id = id;
-        song.version = version;
-
-        let bars_selection = ['start', 'duration'];
-        song.bars = await getSelectionFor(body.bars, bars_selection);
-
-        let beats_selection = ['start', 'duration'];
-        song.beats = await getSelectionFor(body.beats, beats_selection);
-
-        let sections_selection = ['start', 'duration', 'loudness', 'tempo', 'key'];
-        song.sections = await getSelectionFor(body.sections, sections_selection);
-
-        let segments_selection = ['start', 'duration', 'loudness_start', 'pitches', 'timbre'];
-        song.segments = await getSelectionFor(body.segments, segments_selection);
+    return new Promise(function (resolve, reject) {
+        request.get(options, async function (error, res, body) {
+            if(!error && res.statusCode === 200) {
+                resolve(await parseAudioAnalysis(body, id))
+            } else {
+                reject(error)
+            }
+        })
     })
 }
 
+async function parseAudioAnalysis(body, id) {
+    let song = {};
+    song.id = id;
+    song.version = version;
 
-module.exports.getAudioAnalysisForSongID = getAudioAnalysisForSongID;
+    let bars_selection = ['start', 'duration'];
+    song.bars = await getSelectionFor(body.bars, bars_selection);
+
+    let beats_selection = ['start', 'duration'];
+    song.beats = await getSelectionFor(body.beats, beats_selection);
+
+    let sections_selection = ['start', 'duration', 'loudness', 'tempo', 'key'];
+    song.sections = await getSelectionFor(body.sections, sections_selection);
+
+    let segments_selection = ['start', 'duration', 'loudness_start', 'pitches', 'timbre'];
+    song.segments = await getSelectionFor(body.segments, segments_selection);
+
+    return song
+}
+
+async function getAudioAnalysis(id) {
+    return await getAudioAnalysisForATrack(id);
+}
+
+module.exports.getAudioAnalysis = getAudioAnalysis;
