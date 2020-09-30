b7f767e74970b01cc666c5c32a9771ee77c78b90
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-09-30 14:32:55 +0200

    Partly implement song adding function. Add routes for play and pause buttons

--- routes/player.js
+++ routes/player.js
@@ -1,7 +1,11 @@
 var express = require('express');
-let db = require('../modules/mongodb_handler');
-let ah = require('../modules/audio_analysis_handler');
 var router = express.Router();
+
+
+let db = require('../modules/mongodb_handler');
+let audio_analysis_handler = require('../modules/audio_analysis_handler');
+let ESPHandler = require('../modules/esp8266_handler')
+let espHandler = new ESPHandler()
 
 router.get('/playstate', async (req, res, next) => {
     let paused_event = req.query.paused;
@@ -9,12 +13,8 @@ router.get('/playstate', async (req, res, next) => {
     if(paused_state === undefined)
         req.session.paused = paused_event;
     else if (paused_state !== paused_event) {
-        req.session.paused = paused_event
+        req.session.paused = paused_event;
         // TODO: esp handler
-
-
-
-
     }
     res.status(200);
     res.send('ok')
@@ -26,8 +26,10 @@ router.get('/songs/:id', async (req, res, next) => {
     let has_song = await db.hasSong(song_id);
     if(has_song){
         res.status(200);
+        console.log('has song!')
     } else {
         res.status(404);
+        console.log('has not song!')
     }
     res.send();
 });
@@ -36,12 +38,24 @@ router.get('/songs/:id', async (req, res, next) => {
 router.post('/songs/:id', async (req, res, next) => {
     let song_id = req.params.id;
 
-    console.time('Analysis time for id: ' + song_id);
-    await ah.getAudioAnalysisForSongID(song_id);
-    console.timeEnd('Analysis time for id: ' + song_id);
+    //console.time('Analysis time for id: ' + song_id);
+    let song = await audio_analysis_handler.getAudioAnalysis(song_id);
+    await db.addSong(song)
+    console.log('added song!')
+    //console.timeEnd('Analysis time for id: ' + song_id);
     //console.log('Add song: ', song_id);
     res.status(200);
     res.send();
 });
+
+router.get('/playTest', async function (req, res) {
+    ESPHandler.timerTest()
+    res.redirect('/#')
+})
+
+router.get('/pauseTest', async function (req, res) {
+    res.redirect('/#')
+})
+
 
 module.exports = router;
