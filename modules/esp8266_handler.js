8a2a099cb510511506722752f70e4cccfc092231
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-10-09 10:10:17 +0200

    Implement esp8266 handler

--- modules/esp8266_handler.js
+++ modules/esp8266_handler.js
@@ -1,34 +1,132 @@
 let db = require('../modules/mongodb_handler');
+let _ = require('lodash');
 
 class Esp8266_handler {
-    intervall;
-    current_song = '2FL785nGQsw1oaYwM1j6Eh';
 
     constructor() {
+        this.time_delta_ms = 3000;
+        this.setWindow();
+        this.is_paused = true;
+        setInterval(() => {this.timer()}, 10);
     }
 
-    timerTest(start_time){
-        let passed_time = Date.now() - start_time
-        console.log('Current passed time: ' + passed_time)
-        console.log('Current song id: ' + this.current_song)
+    setWindow(){
+        this.window_start = Date.now();
+        this.window_end = this.window_start + this.time_delta_ms;
     }
 
-    sendDataFrom(time_in_ms = 0){
-        const offset = Date.now() - time_in_ms
-        this.intervall = setInterval(this.timerTest.bind(null, offset), 1000)
-    }
+    timer(){
+        if(!this.is_paused){
+            /*
+            console.log('Window_start, current position and window_end: '
+                + (this.window_start - this.position)/1000 + '\t'
+                + (Date.now() - this.position)/1000 + '\t'
+                + (this.window_end - this.position)/1000);
+             */
+            if((Date.now() - this.window_start) > this.time_delta_ms) {
+                this.setWindow();
+                this.sendDataWindow();
+            }
 
-    stopData(){
-        if(this.intervall !== undefined) {
-            clearInterval(this.intervall)
         }
     }
 
-    setCurrentSong(song_id){
-        this.current_song = song_id
+    songInfoHandler(song_id, position, is_paused){
+        let new_song = this.isNewSong(song_id);
+
+        if(new_song){
+            this.newPosition(0);
+            this.sendDataWindow();
+        } else {
+            this.checkPositionDelta(position);
+        }
+        this.playbackStatus(is_paused);
     }
 
 
+    isNewSong(song_id){
+        if(this.song_id === undefined || this.song_id !== song_id) {
+            this.song_id = song_id;
+            db.getSong(this.song_id).then(data => {
+                this.current_song_data = data;
+            });
+            return true
+        } else {
+            return false
+        }
+    }
+
+    checkPositionDelta(position){
+        let relative_position = Date.now() - this.position;
+        let needs_adjustment = Math.abs( relative_position - position) > 100;
+
+        if(this.position === undefined || needs_adjustment)
+            this.newPosition(position);
+
+        return (Date.now() - this.position - position)/1000;
+    }
+
+    newPosition(position){
+        this.position = Date.now() - position;
+        this.setWindow()
+    }
+
+    playbackStatus(is_paused){
+        if(this.is_paused !== is_paused) {
+            this.is_paused = is_paused;
+            if(this.is_paused)
+                this.stopPlayback();
+            else
+                this.startPlayback();
+        }
+    }
+
+
+    startPlayback(){
+        console.log('start playback')
+    }
+
+    stopPlayback(){
+        console.log('stop playback')
+    }
+
+    sendDataWindow(){
+        let assembly = this.assembleDataWindow();
+        console.log(assembly)
+
+
+    }
+
+    assembleDataWindow(){
+        let beats_important = ['start', 'duration'];
+        let beats_filter = this.filterForWindow(this.current_song_data.beats);
+        let beats_selection = this.getSelectionFor(beats_filter, beats_important);
+
+        let segments_important = ['start', 'duration', 'pitches'];
+        let segments_filter = this.filterForWindow(this.current_song_data.segments);
+        let segments_selection = this.getSelectionFor(segments_filter, segments_important);
+
+        let assembly = {};
+        assembly['beats'] = beats_selection;
+        assembly['segments'] = segments_selection;
+
+        return assembly;
+    }
+
+    filterForWindow(song_part){
+        return _.filter(song_part, (part) => {
+            let relative_start = (this.window_start - this.position)/1000;
+            let relative_end = (this.window_end - this.position)/1000;
+            return (part.start >= relative_start && part.start < relative_end)
+        })
+    }
+
+    getSelectionFor(json, selection) {
+        return _.map(json, function (entry) {
+            return _.pick(entry, selection)
+        });
+    }
+
 }
 
-module.exports.ESPHandler = Esp8266_handler
+module.exports = Esp8266_handler;
